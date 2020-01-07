6a750326d12bf05e83410a3f0b4ffd6e98cade70
Qarib Haider <qarib@hugmena.com>
2020-01-07 11:54:54 +0400

    Testing v0.1.1

---
+++ assets/js/script.js
@@ -0,0 +1,45 @@
+window.onload = () => {
+    let places = staticLoadPlaces();
+    renderPlaces(places);
+};
+
+function staticLoadPlaces() {
+    return [
+        {
+            name: 'Magnemite',
+            location: {
+                lat: 25.0905208,
+                lng: 55.1486314,
+            }
+        },
+        {
+            name: 'Magnemite',
+            location: {
+                lat: 25.098015,
+                lng: 55.156384,
+            }
+        },
+    ];
+}
+
+function renderPlaces(places) {
+    let scene = document.querySelector('a-scene');
+
+    places.forEach((place) => {
+        let latitude = place.location.lat;
+        let longitude = place.location.lng;
+
+        let model = document.createElement('a-entity');
+        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+        model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
+        model.setAttribute('rotation', '0 180 0');
+        model.setAttribute('animation-mixer', '');
+        model.setAttribute('scale', '0.5 0.5 0.5');
+
+        model.addEventListener('loaded', () => {
+            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+        });
+
+        scene.appendChild(model);
+    });
+}
